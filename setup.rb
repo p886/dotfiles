22e27c4bf7808984380b8bd7d045ff53386c4be1
Philipp Antar <philippantar@gmail.com>
2015-02-20 20:02:24 +0100

    :bug: fix

--- setup.rb
+++ setup.rb
@@ -3,15 +3,17 @@
 # default location for dotfiles is $HOME
 destination_paths = Hash.new(Dir.home)
 
+dotfiles_dir = File.expand_path('../dotfiles', __FILE__)
+
 # name of entries withing dotfiles dir; without current dir (.) and parent dir (..)
-dotfiles = Dir.entries("dotfiles").reject { |entry| (entry == '.' || entry == '..') }
+dotfiles = Dir.entries(dotfiles_dir).reject { |entry| (entry == '.' || entry == '..') }
 
 dotfiles.each do |dotfile|
-  source = File.join(File.expand_path('../dotfiles', __FILE__), dotfile)
+  source = File.join(dotfiles_dir, dotfile)
   destination = File.join(destination_paths[dotfile], '.' + dotfile)
 
   unless File.exist?(destination)
     File.symlink(source, destination)
-    puts "created symlink for #{dotfile}"
+    puts "created symlink for #{dotfile} at #{destination}"
   end
 end
